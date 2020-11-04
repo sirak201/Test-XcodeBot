252b07d031907b60c804f32c5c5f7a6a3fefa31e
Sirak <szeray@apple.com>
2020-11-04 11:59:32 -0800

    Initial Commit

---
+++ XcodeBot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeBot
+//
+//  Created by Sirak Zeray on 11/4/20.
+//  Copyright © 2020 Sirak Zeray. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeBot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeBot
+//
+//  Created by Sirak Zeray on 11/4/20.
+//  Copyright © 2020 Sirak Zeray. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
